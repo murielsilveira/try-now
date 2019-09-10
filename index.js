8c4f43f0cfe8520ebd80a3c1a009616bf476e712
Muriel Silveira <murielkong@gmail.com>
2019-09-10 22:08:46 +0200

    feat: configure now deployment with multiple paths
    - "/": root with a simple response
    - "/admin": protected by an admin:admin basic auth
    - "/static": serves a static folder

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require('express')
+const basicAuth = require('express-basic-auth')
 
 const app = express()
 app.get('/', (req, res) => {
   res.send('👋')
 })
+
+app.use(
+  '/admin',
+  basicAuth({
+    challenge: true,
+    realm: 'try-now',
+    users: { 'admin': 'admin' },
+    unauthorizedResponse: 'Restricted area. Please login (admin:admin).'
+  }),
+  (req, res) => {
+    res.send('🔥')
+  }
+)
+
+app.use('/static', express.static(__dirname + '/static'))
 
 const port = process.env.PORT || 3030
 app.listen(port, () => {
